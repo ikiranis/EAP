7b918fa8fdbaffe84c9ae3f9f444d12c0c42c86f
rocean <rocean74@gmail.com>
2019-01-16 18:26:19 +0200

    Removed all files that are in the .gitignore

---
+++ cmake-build-debug/CMakeFiles/eap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eap.dir/main.c.o"
+  "eap.pdb"
+  "eap"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/eap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
